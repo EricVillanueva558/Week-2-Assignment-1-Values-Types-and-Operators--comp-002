85076b2e82620c8515b045f668bab92fda8ca4ae
Eric Villanueva <villanuevaeric1305@gmail.com>
2024-10-25 23:19:03 -0700

    updated js

---
+++ script.js
@@ -0,0 +1,29 @@
+// Output "Hello World!" to the console
+console.log("Hello World!");
+
+// Concatenate three strings and output the result to the console
+console.log("Hello" + " " + "World!");
+
+// Output "Hello" on one line and "World!" on the next line with a single console.log
+console.log("Hello\nWorld!");
+
+// Output the result of the arithmetic expression 3 * 8 + 2
+console.log(3 * 8 + 2);
+
+// Output the result of the comparison: 5 is less than 10
+console.log(5 < 10);
+
+// Output the result of the comparison: 10 is greater than 5
+console.log(10 > 5);
+
+// Output the result of the comparison: "Apples" is NOT equal to "Oranges"
+console.log("Apples" !== "Oranges");
+
+// Output the result of the comparison: 10 * 10 is greater than 50 AND 5 * 5 is less than 50
+console.log((10 * 10 > 50) && (5 * 5 < 50));
+
+// Output the result of the comparison: 1 + 2 is greater than 5 OR 1 * 10 is greater than 5
+console.log((1 + 2 > 5) || (1 * 10 > 5));
+
+// Demonstrate an automatic type conversion
+console.log("The number is " + 5);  // JavaScript converts number 5 to string
